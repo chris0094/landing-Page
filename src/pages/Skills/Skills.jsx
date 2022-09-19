0fad49f5d21016cbe62cc7c80491115144b81011
Juan Mario Valderrama <juancho9402@hotmail.com>
2022-09-19 00:46:13 -0500

    Skills page added

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import htmlImg from '../../assets/skills/html5.svg'
+import cssImg from '../../assets/skills/css3.svg'
+import javaImg from '../../assets/skills/java.svg'
+import javascriptImg from '../../assets/skills/javascript.svg'
+import reactImg from '../../assets/skills/react.svg'
+import csharpImg from '../../assets/skills/c-sharp.svg'
+import './Skills.css'
 
 export const Skills = () => {
+  const images = [{
+    title: 'HTML',
+    src: htmlImg,
+  },
+  {
+    title: 'CSS',
+    src: cssImg,
+  },
+  {
+    title: 'Java',
+    src: javaImg,
+  },
+  {
+    title: 'JavaScript',
+    src: javascriptImg,
+  },
+  {
+    title: 'ReactJS',
+    src: reactImg,
+  },
+  {
+    title: 'C Sharp',
+    src: csharpImg,
+  }]
+
+  const listImages = images.map(image => <figure key={image.title.toLocaleLowerCase()}>
+    <img src={image.src} alt={image.title} loading="lazy" />
+    <figcaption>{image.title}</figcaption>
+  </figure>)
+
   return (
-    <div>Skills</div>
+    <section className='skills'>
+      <h1>Habilidades</h1>
+      <div className='skills__items'>
+        {listImages}
+      </div>
+    </section>
   )
 }
