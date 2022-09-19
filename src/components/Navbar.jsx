68fab303b6d531ee771cbea74a369a549a71b653
Juan Mario Valderrama <juancho9402@hotmail.com>
2022-09-19 00:44:36 -0500

    Set router and navbar position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,47 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 import logo from './../assets/img/logo-cris.ico'
 import './Navbar.css'
 
 export const Navbar = () => {
+  const navLink = document.querySelectorAll('a');
+
+
+  const closeMenu = () => {
+    const hamburger = document.querySelector('#hamburger');
+    const navMenu = document.querySelector('.navbar__links');
+    hamburger.classList.remove('open');
+    navMenu.classList.remove('open');
+  }
+
+  const openMenu = () => {
+    const hamburger = document.querySelector('#hamburger');
+    const navMenu = document.querySelector('.navbar__links');
+    hamburger.classList.toggle('open');
+    navMenu.classList.toggle('open');
+  }
+
+  navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
   return (
     <nav className='navbar'>
-        <div className='navbar__logo'>
-            <img src={logo} alt='Logo' />
-        </div>
-        <div className='navbar__links'>
-            <Link to='/'>Inicio</Link>
-            <Link to='/profile'>Perfil</Link>
-            <Link to='/skills'>Habilidades</Link>
-            <Link to='/studies'>Estudios</Link>
-            <Link to='/portfolio'>Portafolio</Link>
-            <Link to='/experience'>Experiencia</Link>
-            <Link to='/contact'>Contacto</Link>
-        </div>
+      <div className='navbar__logo'>
+        <img src={logo} alt='Logo' loading="lazy" />
+      </div>
+      <div className='navbar__links'>
+        <NavLink to='/'>Inicio</NavLink>
+        <NavLink to='/profile'>Perfil</NavLink>
+        <NavLink to='/skills'>Habilidades</NavLink>
+        <NavLink to='/formation'>Estudios</NavLink>
+        <NavLink to='/portfolio'>Portafolio</NavLink>
+        <NavLink to='/experience'>Experiencia</NavLink>
+        <NavLink to='/contact'>Contacto</NavLink>
+      </div>
+      <div className='navbar__hamburger' id='hamburger' onClick={openMenu}>
+        <span className='navbar__bar'></span>
+        <span className='navbar__bar'></span>
+        <span className='navbar__bar'></span>
+      </div>
     </nav >
   )
 }
--- src/routes/AppNavigate.jsx
+++ src/routes/AppNavigate.jsx
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom"
 import React from 'react'
+import './AppNavigate.css'
 
 import { Profile, Formation, Skills, Experience, Portfolio, Contact, Home } from "../pages"
 
 export const AppNavigate = () => {
   return (
-    <Routes>
-      <Route exact path='/' element={<Home />}></Route>
-      <Route path='/profile' element={<Profile />}></Route>
-      <Route path='/skills' element={<Skills />}></Route>
-      <Route path='/formation' element={<Formation />}></Route>
-      <Route path='/experience' element={<Experience />}></Route>
-      <Route path='/portfolio' element={<Portfolio />}></Route>
-      <Route path='/contact' element={<Contact />}></Route>
-    </Routes>
+    <main className="router">
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/skills' element={<Skills />}></Route>
+        <Route path='/formation' element={<Formation />}></Route>
+        <Route path='/experience' element={<Experience />}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
+    </main>
   )
 }
