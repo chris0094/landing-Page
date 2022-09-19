45a2c92ae669b358cb698620f0629c594b5fcb09
Juan Mario Valderrama <juancho9402@hotmail.com>
2022-09-19 00:44:47 -0500

    Home page added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,15 @@ import image from '../../assets/img/start-image.png'
 
 export const Home = () => {
   return (
-    <div className="home">
+    <section className="home">
       <div className='home__presentation'>
         <h1>¡Hola!</h1>
         <h2>Mi nombre es Cristhian,</h2>
         <h3>los invito a conocer mi trabajo.</h3>
       </div>
       <div className='home__image'>
-        <img src={image} alt='Presentation' />
+        <img src={image} alt='Presentation' loading="lazy" />
       </div>
-    </div>
+    </section>
   )
 }
